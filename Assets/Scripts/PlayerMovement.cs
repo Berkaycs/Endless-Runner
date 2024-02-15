388354187a82f16c9e8e50be97061e037a0b8959
berkaypicakci <berkaypicakci@outlook.com>
2024-02-15 03:11:01 +0300

    fourth commit
    - creating and setting animations
    - creating player animator script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float horizontalInput;
     public float horizontalMultiplier = 2;
 
+    public float jumpForce = 6;
+    public float doubleJumpForce = 6;
+
+    public float Gravity = -20;
+
+    public bool isOnGround = true;
+    public bool doubleJump = false;
+
     private Rigidbody rb;
 
     void Awake()
@@ -25,5 +33,35 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("First jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            doubleJump = true;
+        }
+
+        else if (!isOnGround && doubleJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            doubleJump = false;
+            Debug.Log("Second jump");
+            rb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Player collides with the ground");
+            isOnGround = true;
+            doubleJump = false;
+        }
     }
 }
